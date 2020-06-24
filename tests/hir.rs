10fb642aaf3a77061618c5a735260a48a1dda389
lausek <root@lausek.eu>
2020-06-24 17:55:44 +0200

    add macro for implementing tests

--- tests/hir.rs
+++ tests/hir.rs
@@ -1,25 +1,55 @@
 use lovm2::hir::prelude::*;
+use lovm2::Context;
 use lovm2::ModuleBuilder;
 use lovm2::value::RuValue;
 use lovm2::vm::Vm;
 
+#[macro_export]
+macro_rules! define_test {
+    {
+        $( $fname:ident { $( $inx:expr ; )* } )*
+            #ensure $ensure:tt
+    } => {{
+        let mut builder = ModuleBuilder::new();
+        let mut called = std::rc::Rc::new(std::cell::Cell::new(false));
+
+        $(
+            let hir = {
+                let mut hir = HIR::new();
+                $(
+                    hir.push($inx);
+                )*
+                hir
+            };
+            builder.add(stringify!($fname)).hir(hir);
+        )*
+
+        let mut vm = Vm::new();
+        let mut called_ref = called.clone();
+        vm.context_mut().set_interrupt(10, move |ctx| {
+            called_ref.set(true);
+            $ensure(ctx);
+        });
+
+        let module = builder.build().unwrap();
+        vm.load_and_import_all(module).unwrap();
+        vm.run();
+
+        assert!(called.get());
+    }};
+}
+
 #[test]
 fn assign_local() {
-    let mut builder = ModuleBuilder::new();
+    define_test! {
+        main {
+            Assign::local("n".into(), CoValue::Int(4).into());
+            Interrupt::new(10);
+        }
 
-    let mut main_hir = HIR::new();
-    main_hir.push(Assign::local("n".into(), CoValue::Int(4).into()));
-    main_hir.push(Interrupt::new(10));
-
-    builder.add("main").hir(main_hir);
-
-    let mut vm = Vm::new();
-    vm.context_mut().set_interrupt(10, |ctx| {
-        let frame = ctx.frame_mut().unwrap();
-        assert_eq!(RuValue::Int(4), *frame.locals.get("n").unwrap());
-    });
-
-    let module = builder.build().unwrap();
-    vm.load_and_import_all(module).unwrap();
-    vm.run();
+        #ensure (|ctx: &mut Context| {
+            let frame = ctx.frame_mut().unwrap();
+            assert_eq!(RuValue::Int(4), *frame.locals.get("n").unwrap());
+        })
+    }
 }
