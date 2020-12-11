7ff1095fcdefd760114d526065c9c9f6247efd51
lausek <root@lausek.eu>
2020-12-11 23:08:04 +0100

    refactor tests

--- tests/mod.rs
+++ tests/mod.rs
@@ -6,3 +6,9 @@ pub mod hir;
 pub mod lir;
 pub mod module;
 pub mod value;
+
+pub use lovm2::context::Context;
+pub use lovm2::module::Module;
+pub use lovm2::prelude::*;
+pub use lovm2::value::Value;
+pub use lovm2::vm::Vm;
