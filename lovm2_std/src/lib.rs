202248389fce2a1fe4ba13805fb9d9ce1bc670a7
lausek <root@lausek.eu>
2020-12-22 15:12:24 +0100

    publish all functions in docs

--- lovm2_std/src/lib.rs
+++ lovm2_std/src/lib.rs
@@ -2,3 +2,8 @@ pub use lovm2_std_collection;
 pub use lovm2_std_json;
 pub use lovm2_std_net;
 pub use lovm2_std_string;
+
+pub use lovm2_std_collection::*;
+pub use lovm2_std_json::*;
+pub use lovm2_std_net::*;
+pub use lovm2_std_string::*;
