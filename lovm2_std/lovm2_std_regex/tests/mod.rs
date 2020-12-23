a00503e4f5dc798c89835a31daf0b7fb26a356f6
lausek <root@lausek.eu>
2020-12-23 10:54:42 +0100

    add tests for regex module

--- lovm2_std/lovm2_std_regex/tests/mod.rs
+++ lovm2_std/lovm2_std_regex/tests/mod.rs
@@ -48,7 +48,25 @@ fn native_is_match() {
 
 #[test]
 fn native_captures() {
-    let mut vm = run_module_test(|_| {});
+    let re = &lv2_var!(re);
 
-    assert!(false);
+    let mut vm = run_module_test(|builder| {
+        builder
+            .add("init")
+            .step(Assign::global(re, lv2_call!(new_regex, "(\\d)?(\\S+)")));
+    });
+
+    vm.call("init", &[]).unwrap();
+
+    let re = vm.context_mut().value_of(re).unwrap().clone();
+
+    let result = vm.call("captures", &[re.clone(), "1abc".into()]).unwrap();
+    assert_eq!(Value::from("1abc"), result.get(&Value::Int(0)).unwrap());
+    assert_eq!(Value::from("1"), result.get(&Value::Int(1)).unwrap());
+    assert_eq!(Value::from("abc"), result.get(&Value::Int(2)).unwrap());
+
+    let result = vm.call("captures", &[re.clone(), "abc".into()]).unwrap();
+    assert_eq!(Value::from("abc"), result.get(&Value::Int(0)).unwrap());
+    assert_eq!(Value::Nil, result.get(&Value::Int(1)).unwrap());
+    assert_eq!(Value::from("abc"), result.get(&Value::Int(2)).unwrap());
 }
