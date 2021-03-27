254848f639a995d8a1355711b4d2e7c08a179b6f
lausek <root@lausek.eu>
2021-03-27 10:41:16 +0100

    update valid path docs

--- src/lovm2_core/src/gen/opt/valid.rs
+++ src/lovm2_core/src/gen/opt/valid.rs
@@ -26,10 +26,13 @@ impl ValidPath {
 
     fn update(&mut self, code: &[LirElement], mut off: usize, mut scanning: bool) {
         while let Some(elem) = code.get(off) {
+            // Stop adding offsets to valid path if the current offset is already known.
             if self.is_valid(off) {
                 break;
             }
 
+            // Entry labels must be reachable from extern calls and are always valid as such.
+            // Everything that follows the entry will be added to the valid path.
             if let LirElement::Entry { .. } = elem {
                 scanning = true;
             }
@@ -40,6 +43,7 @@ impl ValidPath {
 
             match elem {
                 LirElement::Jump { condition, label } => {
+                    // Jumps change the programs execution location and must be handled carefully.
                     let (label_off, _) = code
                         .iter()
                         .enumerate()
@@ -47,13 +51,21 @@ impl ValidPath {
                         .unwrap();
 
                     if condition.is_some() {
+                        // If the jump is conditionally executed, we interrupt scanning and check
+                        // the target offset first as it could be reached by the program.
+                        // Once this scan is done we continue scanning at the current location.
                         self.update(code, label_off, true);
                     } else {
+                        // If the jump is always executed (does not have a condition), we
+                        // continue scanning at the jumps target offset.
                         off = label_off;
                         continue;
                     }
                 }
                 LirElement::Ret => {
+                    // Return instructions mark a functions end. Instructions after return must be
+                    // explicitly targeted by jumps or follow an entry label in order to be
+                    // considered reachable.
                     scanning = false;
                 }
                 _ => {}
